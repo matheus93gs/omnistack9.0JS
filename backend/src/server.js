3463aaccd203a889bc57bc04cbeadda3e37457ab
gr8matheus <gr8.matheus@gmail.com>
2019-10-09 10:52:53 -0300

    nodemon

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app =  express();
 
-app.get('/')
+//GET pega informação
+//POST insere a informação
+//PUT altera informação
+//DELETE apaga informação
+
+app.get('/', (req, res) => {
+    return res.json({ message: "Hello World" });
+});
 
 app.listen(3001);
-
-
